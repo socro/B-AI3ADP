4c052605bb34d58b1eb5478718f77cc9744cb696
Rene <Rene@Rene-PC>
2015-11-11 23:40:31 +0100

    - I push it

--- src/sort/Benchmark.java
+++ src/sort/Benchmark.java
@@ -1,17 +1,69 @@
 package sort;
 
 import interfaces.AdtArray;
-import static org.junit.Assert.*;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class Benchmark {
+    
+    private static boolean firstrun = true;
+    private static String nl = System.lineSeparator();
 
-    public static void main(String args[]){
-        Generator.sortnum(15);
+    public static void main(String args[]) {
+        Generator.sortnum(1500);
         AdtArray array1 = Generator.importNums("zahlen.dat");
-        AdtArray array2 = Generator.importNums("zahlen.dat");
-        Sorter.quicksort(array1, (int start, int end) -> (start+end)/2);
-        Sorter.insertionsort(array2, 0, array2.length());
+
+        outputToCSV("QuicksortTime + IO1",Sorter.quicksortSteps(array1, (int start, int end) -> (end)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO2",Sorter.quicksortSteps(array1, (int start, int end) -> (end-1)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO3",Sorter.quicksortSteps(array1, (int start, int end) -> (end-2)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO4",Sorter.quicksortSteps(array1, (int start, int end) -> (end-3)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO5",Sorter.quicksortSteps(array1, (int start, int end) -> (end-4)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO6",Sorter.quicksortSteps(array1, (int start, int end) -> (end-5)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO7",Sorter.quicksortSteps(array1, (int start, int end) -> (end-6)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO8",Sorter.quicksortSteps(array1, (int start, int end) -> (end-7)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO9",Sorter.quicksortSteps(array1, (int start, int end) -> (end-8)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO10",Sorter.quicksortSteps(array1, (int start, int end) -> (end-9)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
+        outputToCSV("QuicksortTime + IO11",Sorter.quicksortSteps(array1, (int start, int end) -> (end-10)),Sorter.quicksortTime(array1, (int start, int end) -> (end)));
         //----------------------------------------
         System.out.println("Ende vom Benchmark");
+    }
+
+    public static void outputToCSV(String filename,long[] ioTest,long[] runtimeTest) {
+        if(firstrun){
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(filename, true), "utf-8"))) {
+                writer.write("Testname,AlgRQ,AlgWQ,AdtRQ,AdtWQ,AlgRI,AlgWI,AdtRI,AdtWI,RtQ,RtI");
+                writer.write(nl);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            firstrun= false;
+        }
+
+        long[] args = new long[runtimeTest.length+ioTest.length];
+        System.arraycopy(ioTest, 0, args, 0, ioTest.length);
+        System.arraycopy(runtimeTest, 0, args, ioTest.length, runtimeTest.length);
+        
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(filename, true), "utf-8"))) {
+                writer.write(filename + ",");
+            // print line
+            for (int i = 0; i < args.length; i++) {
+                if (i != args.length - 1) {
+                    writer.write(args[i] + ",");
+                } else {
+                    writer.write("" + args[i]);
+                }
+
+            }
+            writer.write(nl);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/sort/Sorter.java
+++ src/sort/Sorter.java
@@ -4,12 +4,26 @@ import interfaces.AdtArray;
 
 public class Sorter {
 
-    private static int noMagicNumber = 12;
+    private static final int AlgRQ = 0;
+    private static final int AlgWQ = 1;
+    private static final int AdtRQ = 2;
+    private static final int AdtWQ = 3;
+    private static final int AlgRI = 4;
+    private static final int AlgWI = 5;
+    private static final int AdtRI = 6;
+    private static final int AdtWI = 7;
+
+    private static int noMagicNumber = 42;
     private static long[] timeA = new long[2];
-    private static long[] stepsA = new long[2];
+    private static long[] stepsA = new long[8];
+    private static long insertionTime;
     private static long steps = 0;
 
-    private Sorter(){}
+    private static boolean subroutine = false;
+
+    private Sorter() {
+    }
+
     public static void insertionsort(AdtArray array, int begin, int end) {
         if (begin < 1) {
             begin = 1;
@@ -30,73 +44,224 @@ public class Sorter {
             array.set(j, temp);
         }
     }
-    
-    public static void quicksort(AdtArray array, PivotMethod pivot){
+
+    public static void quicksort(AdtArray array, PivotMethod pivot) {
         quicksort_(array, pivot, 0, array.length());
     }
 
     public static void quicksort_(AdtArray array, PivotMethod pivot, int left, int right) {
-        int pivotIndex = pivot.getPivotIndex(left, right);
-        if( left < right){
-            int i = quickswap(array, left, right, pivotIndex);
-            
-            quicksort_(array, pivot, left, i-1);
-            quicksort_(array, pivot, i+1, right);
+        if (right - left < noMagicNumber) {
+            insertionsort(array, left, right);
+        } else {
+            int pivotIndex = pivot.getPivotIndex(left, right);
+            if (left < right) {
+                int i = quickswap(array, left, right, pivotIndex);
+
+                quicksort_(array, pivot, left, i - 1);
+                quicksort_(array, pivot, i + 1, right);
+            }
         }
     }
-    private static int quickswap(AdtArray array, int ilinks, int irechts, int pivotIndex){
+
+    private static int quickswap(AdtArray array, int ilinks, int irechts, int pivotIndex) {
         int i = ilinks;
-        int j = irechts-1;
-        int pivot = pivotIndex;
-        
-        while(i<=j){
-            while((array.get(i) <= pivot) && (i < irechts)){
+        int j = irechts - 1;
+        int pivot = array.get(pivotIndex);
+
+        while (i <= j) {
+            while ((array.get(i) <= pivot) && (i < irechts)) {
                 i++;
             }
-            while((ilinks <= j) && (array.get(j) > pivot)){
+            while ((ilinks <= j) && (array.get(j) > pivot)) {
                 j--;
             }
-            if(i < j){
+            if (i < j) {
                 int temp = array.get(i);
-                array.set(i, j);
+                array.set(i, array.get(j));
                 array.set(j, temp);
             }
         }
         int temp = array.get(i);
-        array.set(i,array.get(irechts));
+        array.set(i, array.get(irechts));
         array.set(irechts, temp);
-        
+
         return i;
     }
 
     public static long insertionsortTime(AdtArray array, int begin, int end) {
-        long time = System.currentTimeMillis();
+        insertionTime = System.currentTimeMillis();
         insertionsort(array, begin, end);
-        return time - System.currentTimeMillis();
+        insertionTime = System.currentTimeMillis() - insertionTime;
+        return insertionTime;
     }
 
+    /**
+     *
+     * @param array
+     * @param pivot
+     * @return long[0] = quicksorttime | long[1] = insertiontime
+     */
     public static long[] quicksortTime(AdtArray array, PivotMethod pivot) {
-        timeA[0] = 0;
+        timeA[0] = System.currentTimeMillis();
         timeA[1] = 0;
-        quicksort(array, pivot);
+
+        quicksortTime_(array, pivot, 0, array.length());
+
+        timeA[1] = insertionTime;
+        timeA[0] = System.currentTimeMillis() - timeA[0] - insertionTime;
         return timeA;
     }
 
-    public static long insertionsortSteps(AdtArray array, int begin, int end) {
-        steps = 0;
-        insertionsort(array, begin, end);
-        return steps;
+    private static void quicksortTime_(AdtArray array, PivotMethod pivot, int left, int right) {
+        if (right - left < noMagicNumber) {
+            insertionsortTime(array, left, right);
+        } else {
+            int pivotIndex = pivot.getPivotIndex(left, right);
+            if (left < right) {
+                int i = quickswap(array, left, right, pivotIndex);
+
+                quicksortTime_(array, pivot, left, i - 1);
+                quicksortTime_(array, pivot, i + 1, right);
+            }
+        }
     }
 
+    /**
+     *
+     * @param array
+     * @param begin
+     * @param end
+     * @return stepsA[AlgRI] = readaccess Algo | stepsA[AlgWI] = writeaccess
+     * Algo | stepsA[AdtRI] = readaccess Adt | stepsA[AdtWI] = writeaccess Adt
+     */
+    public static long[] insertionsortSteps(AdtArray array, int begin, int end) {
+        if (!subroutine) {
+            clearStepsArray();
+        }
+
+        if (begin < 1) {
+            begin = 1;
+            stepsA[AlgWI]++;
+        }
+
+        if (end > array.length()) {
+            end = array.length();
+            stepsA[AlgWI]++;
+            stepsA[AdtRI]++;
+        }
+
+        stepsA[AlgWI]++;
+
+        for (int i = begin; i <= end; i++) {
+            int j = i;
+            int temp = array.get(i);
+            stepsA[AlgWI]++;
+            stepsA[AdtRI]++;
+
+            while (j > 0 && array.get(j - 1) > temp) {
+                stepsA[AdtRI]++;
+                array.set(j, array.get(j - 1));
+                stepsA[AdtWI]++;
+                stepsA[AdtRI]++;
+                j--;
+            }
+            array.set(j, temp);
+            stepsA[AdtWI]++;
+        }
+        long[] returnValue = new long[8];        
+        System.arraycopy( stepsA, 0, returnValue, 0, stepsA.length );
+        if (!subroutine) {
+            clearStepsArray();
+        }
+        return returnValue;
+    }
+
+    /**
+     *
+     * @param array
+     * @param pivot
+     * @return stepsA[AlgRQ] = readaccess Adt Quick | stepsA[AlgWQ] =
+     * writeaccess Adt Quick | stepsA[AdtRQ] = readaccess Adt Quick |
+     * stepsA[AdtWQ] = writeaccess Adt Quick
+     */
     public static long[] quicksortSteps(AdtArray array, PivotMethod pivot) {
-        stepsA[0] = 0;
-        stepsA[1] = 0;
-        quicksort(array, pivot);
-        return stepsA;
+        clearStepsArray();
+
+        quicksortSteps_(array, pivot, 0, array.length());
+        
+        long[] returnValue = new long[8];
+        System.arraycopy( stepsA, 0, returnValue, 0, stepsA.length );
+        clearStepsArray();
+        return returnValue;
+    }
+
+    public static void clearStepsArray() {
+        for (int i = 0; i < stepsA.length; i++) {
+            stepsA[i] = 0;
+        }
+    }
+
+    private static void quicksortSteps_(AdtArray array, PivotMethod pivot, int left, int right) {
+        if (right - left < noMagicNumber) {
+            subroutine = true;
+            insertionsortSteps(array, left, right);
+            subroutine = false;
+        } else {
+            int pivotIndex = pivot.getPivotIndex(left, right);
+            stepsA[AlgWQ]++;
+            if (left < right) {
+                int i = quickswapSteps(array, left, right, pivotIndex);
+                stepsA[AlgWQ]++;
+                quicksortSteps_(array, pivot, left, i - 1);
+                quicksortSteps_(array, pivot, i + 1, right);
+            }
+        }
+    }
+
+    private static int quickswapSteps(AdtArray array, int ilinks, int irechts, int pivotIndex) {
+        int i = ilinks;
+        stepsA[AlgWQ]++;
+        int j = irechts - 1;
+        stepsA[AlgWQ]++;
+        int pivot = array.get(pivotIndex);
+        stepsA[AdtRQ]++;
+        stepsA[AlgWQ]++;
+
+        while (i <= j) {
+            while ((array.get(i) <= pivot) && (i < irechts)) {
+                i++;
+                stepsA[AdtRQ]++;
+            }
+            while ((ilinks <= j) && (array.get(j) > pivot)) {
+                j--;
+                stepsA[AdtRQ]++;
+            }
+            if (i < j) {
+                int temp = array.get(i);
+                stepsA[AdtRQ]++;
+                stepsA[AlgWQ]++;
+                array.set(i, array.get(j));
+                stepsA[AdtRQ]++;
+                stepsA[AdtWQ]++;
+                array.set(j, temp);
+                stepsA[AdtWQ]++;
+            }
+        }
+        int temp = array.get(i);
+        stepsA[AdtRQ]++;
+        stepsA[AlgWQ]++;
+        array.set(i, array.get(irechts));
+        stepsA[AdtRQ]++;
+        stepsA[AdtWQ]++;
+        array.set(irechts, temp);
+        stepsA[AdtWQ]++;
+
+        return i;
     }
 
     @FunctionalInterface
     public interface PivotMethod {
+
         int getPivotIndex(int start, int end);
     }
 }
